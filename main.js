3835561b28e02974f9e75af55b28a984bc24acc8
nguyenngocanh <nguyenngocanh4193@gmail.com>
2021-03-28 11:21:44 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,6 @@
+function changeOpenClose() {
+    const changeOpenClose = document.querySelector('#iconMobile');
+    if (changeOpenClose) {
+        changeOpenClose.classList.toggle('top-nav-menu-list-2-open-close')
+    }
+}
